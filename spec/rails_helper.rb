a03537d314a33ba153e458396ee4f2af64dabb00
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-25 17:29:22 -0700

    Add VCR to tests.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,11 +6,20 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-# require 'vcr'
-# require 'webmock/rspec'
+require 'vcr'
+require 'webmock/rspec'
 require 'database_cleaner'
 require 'simplecov'
 require 'factory_bot_rails'
+
+VCR.configure do |config|
+  config.ignore_localhost = true
+  config.allow_http_connections_when_no_cassette = true
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data("<NASA_KEY>") { ENV['NASA_KEY'] }
+end
 
 # WebMock.disable_net_connect!(allow: ['http://localhost:3000/'])
 
--- spec/services/nasa_service_spec.rb
+++ spec/services/nasa_service_spec.rb
@@ -2,14 +2,21 @@ require 'rails_helper'
 
 describe 'NASA Service'do
   it' exists' do
-    service = NasaService.new
-    expect(service).to be_a(NasaService)
+    VCR.use_cassette("nasa_service") do
+      
+      service = NasaService.new
+
+      expect(service).to be_a(NasaService)
+    end
   end
   it 'returns hubble images' do
-    service = NasaService.new
-    
-    expect(service.get_hubble_images.class).to eq(Array)
-    expect(service.get_hubble_images[0][:data][0]).to have_key(:title)
-    expect(service.get_hubble_images[0][:links][0]).to have_key(:href)
+    VCR.use_cassette("nasa_service") do
+
+      service = NasaService.new
+
+      expect(service.get_hubble_images.class).to eq(Array)
+      expect(service.get_hubble_images[0][:data][0]).to have_key(:title)
+      expect(service.get_hubble_images[0][:links][0]).to have_key(:href)
+    end
   end
 end
