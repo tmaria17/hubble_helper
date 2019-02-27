f6b95c186f76275b8d8596c83b53dcd5393cb627
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-27 02:34:48 -0700

    Render Partial and add tags for jquery.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,7 @@
 class PhotosController < ApplicationController
   def index
+    user = User.first
+    @favorites = user.favorites
     hubble_facade = HubbleFacade.new
     @photos = hubble_facade.get_images
   end
