bdf055d89f5e41fc25a33e26077898780e6649ed
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-26 21:52:52 -0700

    Add VCR to facade spec.

--- spec/facades/hubble_facade_spec.rb
+++ spec/facades/hubble_facade_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 describe 'Hubble Facade' do
   it 'returns an array of photos' do
-    hubble_facade = HubbleFacade.new
+    VCR.use_cassette("hubble_facade") do
 
-    expect(hubble_facade.get_images.class).to eq(Array)
-    expect(hubble_facade.get_images[0]).to be_a(Photo)
-    expect(hubble_facade.get_images.first.title).to be_a(String)
-    expect(hubble_facade.get_images.first.url).to be_a(String)
+      hubble_facade = HubbleFacade.new
+
+      expect(hubble_facade.get_images.class).to eq(Array)
+      expect(hubble_facade.get_images[0]).to be_a(Photo)
+      expect(hubble_facade.get_images.first.title).to be_a(String)
+      expect(hubble_facade.get_images.first.url).to be_a(String)
+    end
   end
 end
